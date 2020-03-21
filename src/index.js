1542977f222c3b73290b2c934ba29db9264b7da1
yusra <github email address>
2020-03-21 07:57:09 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetchImg()
+})
+
+function fetchImg() {
+    return fetch(imgUrl)
+        .then(response => response.json())
+        .then(json => renderImg(json.message))
+}
+function renderImg(imgArr) {
+    imgArr.map(imgUrl => {
+        let img = document.createElement('img')
+        img.src = imgUrl
+        document.getElementById("dog-image-container").appendChild(img)
+    })
+}
+
